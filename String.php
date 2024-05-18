8d446974bb2cfde4f87f1736b478f693dd75cdcc
sajibmia35 <sajibmia35@gmail.com>
2024-05-18 19:43:15 +0600

    Version:1.0

---
+++ String.php
@@ -0,0 +1,25 @@
+<?php
+function processString($string) {
+  $vowels = "aeiouAEIOU";
+  $vowelCount = 0;
+  $reversedString = strrev($string);
+
+  for ($i = 0; $i < strlen($string); $i++) {
+    if (strpos($vowels, $string[$i]) !== false) {
+      $vowelCount++;
+    }
+  }
+
+  return [
+    "original" => $string,
+    "vowelCount" => $vowelCount,
+    "reversed" => $reversedString,
+  ];
+}
+
+$strings = ["Hello", "World", "PHP", "Programming"];
+
+foreach ($strings as $string) {
+  $processedString = processString($string);
+  echo "Original String: " . $processedString["original"] .", Vowels Count: " . $processedString["vowelCount"] . ", Reversed String: " . $processedString["reversed"] .PHP_EOL;
+}
